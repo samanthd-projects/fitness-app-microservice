227896cea4b221da5309d9d50a67d4f14d2876c3
ns4024647@gmail.com <samanthd2004@gmail.com>
2025-12-06 16:13:09 +0530

    fitness application

--- fitness-app-frontend/src/authConfig.js
+++ fitness-app-frontend/src/authConfig.js
@@ -1,5 +1,5 @@
 export const authConfig = {
-    clientId: 'oauth2-pkce-client',
+    clientId: 'fitness-client',
     authorizationEndpoint: 'http://localhost:8181/realms/fitness-oauth2/protocol/openid-connect/auth',
     tokenEndpoint: 'http://localhost:8181/realms/fitness-oauth2/protocol/openid-connect/token',
     redirectUri: 'http://localhost:5173',
